3ff92b98c0c1aab5869611709527202e8c33d9a9
Gabriel Coelho <51426003+gs-coelho@users.noreply.github.com>
2020-06-26 18:40:03 -0300

    Adicionando os arquivos na raíz

---
+++ index.js
@@ -0,0 +1,47 @@
+let btnAdicionarAbelha = document.querySelector('#adicionarAbelha');
+let btnRemoverAbelha = document.querySelector('#removerAbelha');
+let containerAbelhasEl = document.querySelector('#containerAbelhas');
+
+class Abelha {
+    constructor () {
+        this.x = 0;
+        this.y = 0;
+        this.baseX = 0;
+        this.baseY = Math.random() * window.innerHeight;
+
+        this.element = document.createElement('img');
+        this.element.src = 'assets/bee.gif';
+        this.element.style.position = 'absolute';
+        this.element.style.top = '0px';
+        this.element.style.left = '-50px';
+        this.element.style.width = '50px';
+
+        this.node = containerAbelhasEl.appendChild(this.element);
+    }
+}
+
+let abelhas = [new Abelha()];
+
+btnAdicionarAbelha.addEventListener('click', () => {
+    abelhas.push(new Abelha());
+});
+
+btnRemoverAbelha.addEventListener('click', () => {
+    abelhas.pop().node.remove();
+});
+
+setInterval(() => {
+    for(let abelha of abelhas){
+        abelha.x += 2;
+        abelha.y = abelha.baseY + Math.sin(abelha.x / 20) * 20;
+
+        let strPosicao = `translate(${abelha.x}px, ${abelha.y}px)`;
+
+        abelha.element.style.transform = strPosicao;
+
+        if(abelha.x > window.innerWidth + 50){
+            abelha.x = 0;
+            abelha.baseY = Math.random() * window.innerHeight;
+        }
+    }
+}, 17);
